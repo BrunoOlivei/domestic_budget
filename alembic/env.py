88e8f7a784de6237cfb5b7c9d9583688a642c7a8
Bruno Oliveira <brunooliveiradiblab@gmail.com>
2026-01-01 20:59:52 +0000

    feat/US-0001 - migrations

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import pool
 from alembic import context
 from app.core.core import get_settings
 from app.core.database.base import Base
+from app.modules.finance.models.accounts import Accounts  # noqa: F401
 
 config = context.config
 if config.config_file_name is not None:
@@ -17,17 +18,6 @@ target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -41,13 +31,8 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
     connectable = config.attributes.get("connection", None)
+
     if connectable is None:
         from sqlalchemy import create_engine
 
--- app/routes/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.routes.bank_account import router as bank_account_router
-
-__all__ = ["bank_account_router"]
