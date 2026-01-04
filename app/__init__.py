a621ed22c039e1991e426721b80eee2b703462b9
Bruno Oliveira <brunooliveiradiblab@gmail.com>
2026-01-04 15:15:44 +0000

    feat/account create route and exception handlers

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
 from app.core.core import get_settings
 from app.core.database import get_database
-# from app.modules.finance import finance_routes
+from app.utils.exception_handlers import configure_exception_handlers
+from app.modules.finance import router as finance_routers
 
 settings = get_settings()
+
+
 app = FastAPI(
     title=settings.api_title,
     description=settings.api_description,
     version=settings.api_version,
-    dependecies=[get_database],
+    dependencies=[Depends(get_database)],
 )
 
-# app.include_router(*finance_routes)
+configure_exception_handlers(app)
+
+
+app.include_router(finance_routers)
